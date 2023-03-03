cd4620fd273efd6b00d04b0002a64ef6f23f98cb
Albert <tita.albert.sebastian@gmail.com>
2023-03-03 09:42:41 +0200

    -
    Signed-off-by: Albert <tita.albert.sebastian@gmail.com>

--- src/suma.java
+++ src/suma.java
@@ -1,5 +1,7 @@
 public class suma {
     public static void sumaNr(int a, int b){
+        System.out.println("a= "+ a);
+        System.out.println("b= "+ b);
         System.out.println("Suma numerelor: "+ (a+b));
 
     }
